f5c5b6e9bc89105f46d44b50830fe713cc9ba4db
bastien dubray <bastien.dubray@gmail.com>
2024-03-26 11:50:52 +0100

    finish list controller & view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,6 @@
 class ListsController < ApplicationController
+  before_action :list, only: [:show]
+
 
   def index
     @lists = List.all
@@ -7,5 +9,28 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     @bookmarks = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(params_lists)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list
+    @list = List.find(params[:id])
+  end
+
+  def params_lists
+    params.require(:list).permit(:name)
   end
 end
